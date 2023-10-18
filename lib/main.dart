a7d7a97fe549b25cd6ca4ef32bd47c59e4719bc9
thetahayaseen <thetahayaseen>
2023-10-18 09:00:46 -0700

    Styled Text

--- lib/main.dart
+++ lib/main.dart
@@ -70,12 +70,17 @@ class NameCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!
+        .copyWith(color: theme.colorScheme.onPrimary);
 
     return Card(
       color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(randomWordPair.asCamelCase),
+        child: Text(
+          randomWordPair.asCamelCase,
+          style: style,
+        ),
       ),
     );
   }
