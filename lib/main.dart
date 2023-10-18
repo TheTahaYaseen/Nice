2f0d48b3767ebfc96447f93ba93030e9cee09cf7
thetahayaseen <thetahayaseen>
2023-10-18 08:21:09 -0700

    Define Nice App Class Using 'Stl Shortcut'

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(NiceApp());
 }
+
+class NiceApp extends StatelessWidget {
+  const NiceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
